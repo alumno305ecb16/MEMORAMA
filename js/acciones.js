28291da0a3853b03b2dbb160597d30f032f1df35
alumno305ecb16 <scorpions130295@gmail.com>
2015-07-01 00:48:18 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(document).ready(function(e) {	
-//document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){
 var puntosb=0;
 var puntosm=0;
 var puntostot=0;
@@ -1177,5 +1177,5 @@ function imageninicio()
 	
 }
 
-//}); 
+}); 
 });
